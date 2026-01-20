e8aeda189a94824b7957f9d7a87fd65248320a5a
urmalchide <chideurmal03@gmail.com>
2026-01-20 18:54:10 +0530

    Initial Spring Boot setup

--- ems-frontend/src/App.js
+++ ems-frontend/src/App.js
@@ -5,22 +5,35 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Box } from '@mui/material';
 import AdminSidebar from './Components/Layout/AdminSidebar';
 import AdminDashboard from './Pages/Admin/AdminDashboard';
+import Login from './Components/LoginEMS/login'
 
-function App() {
-  return (
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Box sx={{ display: 'flex', minHeight: '100vh' }}>
+//         <AdminSidebar />
+//         <Box component="main" sx={{ flexGrow: 1, overflow: 'auto' }}>
+//           <Routes>
+//             <Route path="/admin" element={<Navigate to="/admin/dashboard" replace />} />
+//             <Route path="/admin/dashboard" element={<AdminDashboard />} />
+            
+//           </Routes>
+//         </Box>
+//       </Box>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+
+function App(){
+  return(
     <BrowserRouter>
-      <Box sx={{ display: 'flex', minHeight: '100vh' }}>
-        <AdminSidebar />
-        <Box component="main" sx={{ flexGrow: 1, overflow: 'auto' }}>
-          <Routes>
-            <Route path="/admin" element={<Navigate to="/admin/dashboard" replace />} />
-            <Route path="/admin/dashboard" element={<AdminDashboard />} />
-            {/* Add other admin routes here */}
-          </Routes>
-        </Box>
-      </Box>
-    </BrowserRouter>
-  );
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+      </Routes>
+   </BrowserRouter>
+  )
 }
-
 export default App;
